88af3afac951ad9dae81bed3599f01eca67a2e9b
Scott Jordan <scottjordanswimming@gmail.com>
2019-09-14 18:23:43 +0000

    Automatically backed up by Learn

--- lib/until.rb
+++ lib/until.rb
@@ -3,7 +3,7 @@ def using_until
   
   
   until levitation_force == 10 
-  puts "Wingardium Leviosa Wingardium Leviosa Wingardium Leviosa Wingardium Leviosa"
+  puts "Wingardium Leviosa"
   levitation_force += 1 
   end
 end
